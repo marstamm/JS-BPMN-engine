04cb14f5146f9380d73805a771275044c73b7fdf
Martin Stamm <martin.stamm@camunda.com>
2020-04-09 10:24:23 +0200

    add more elements

--- src/index.js
+++ src/index.js
@@ -1,58 +1,92 @@
-import BpmnModdle from 'bpmn-moddle';
-import testXml from './xml';
+import BpmnModdle from "bpmn-moddle";
+import BpmnJS from "bpmn-js";
+import testXml from "./xml";
 
-
-var moddle = new BpmnModdle();
-moddle.fromXML(testXml, function(err, definitions) {
-
-  console.log(definitions);
-  // Traverse the Process
-  var elements = definitions.rootElements[0].flowElements;
-  console.log(elements);
-  const startEvent = elements.find(el => {
-    // console.log(el);
-    return el.$type === 'bpmn:StartEvent';
-  });
-
-  const idToElementMap = {};
-  elements.forEach(element => {
-    idToElementMap[element.id] = element;
-  });
-
-
-  console.log('_____________Traversing__________')
-
-  let current = startEvent;
-  console.log(current.id, current)
-
-  while(current = current.outgoing && current.outgoing[0].targetRef) {
-    console.log(current.id, current);
-
-    if(current.$type === 'bpmn:ScriptTask') {
-      eval(current.script)
-    }
-  }
-
-  console.log('_____________Done__________')
-
-
-
-  // console.log(idToElementMap);
-  // console.log(startEvent);
-
-
-
-  // // update id attribute
-  // definitions.set('id', 'NEW ID');
-
-  // // add a root element
-  // var bpmnProcess = moddle.create('bpmn:Process', { id: 'MyProcess_1' });
-  // definitions.get('rootElements').push(bpmnProcess);
-
-  // moddle.toXML(definitions, function(err, xmlStrUpdated) {
-
-  //   // xmlStrUpdated contains new id and the added process
-
-  // });
-
+var viewer = new BpmnJS({
+  container: document.getElementById("bpmn"),
 });
+
+document.upload = async function (evt) {
+  console.log(evt, document.getElementById("bpmn-file"));
+  const bpmn = await document.getElementById("bpmn-file").files[0].text();
+  loadViewer(bpmn);
+};
+
+// function addOverlays() {
+
+// }
+
+function loadViewer(bpmnXml) {
+  viewer.importXML(bpmnXml, async function (err, definitions) {
+    // Traverse the Process
+    console.log(err, definitions);
+
+    var elements = viewer._definitions.rootElements[0].flowElements;
+    const startEvent = elements.find((el) => {
+      return el.$type === "bpmn:StartEvent";
+    });
+
+    const idToElementMap = {};
+    elements.forEach((element) => {
+      idToElementMap[element.id] = element;
+    });
+
+    const readyElements = [];
+    let current;
+    readyElements.push(...startEvent.outgoing);
+
+    const processVariables = {};
+
+    while ((current = readyElements.shift())) {
+      current = current.targetRef;
+      console.log(current);
+      if (current.$type === "bpmn:ScriptTask") {
+        var result = async function (str) {
+          await eval(current.script);
+        }.call(processVariables);
+        await result;
+      }
+
+      if (current.$type === "bpmn:ParallelGateway") {
+        current.waitingTokens = current.waitingTokens
+          ? current.waitingTokens + 1
+          : 1;
+
+        if (current.incoming.length > current.waitingTokens) {
+          continue;
+        }
+        current.waitingTokens -= current.incoming.length;
+      }
+
+      if (current.$type === "bpmn:ExclusiveGateway") {
+        let defaultPath;
+        var possibleGateways = current.outgoing.filter((val) => {
+          if (
+            !val.conditionExpression ||
+            (val.conditionExpression && !val.conditionExpression.body)
+          ) {
+            // Probably default path
+            defaultPath = val;
+            return false;
+          }
+          return function () {
+            return eval(val.conditionExpression.body);
+          }.call(processVariables);
+        });
+
+        if (!possibleGateways.length && !defaultPath) {
+          console.error("No possible Path found at element " + current);
+          continue;
+        }
+        readyElements.push(possibleGateways[0] || defaultPath);
+        continue;
+      }
+
+      // Default behaviour
+
+      if (current.outgoing && current.outgoing[0].targetRef) {
+        readyElements.push(...current.outgoing);
+      }
+    }
+  });
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,17 @@
-const webpack = require('webpack');
-const path = require('path');
+const webpack = require("webpack");
+const path = require("path");
+const CopyPlugin = require("copy-webpack-plugin");
 
 const config = {
-  entry: './src/index.js',
+  entry: "./src/index.js",
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'main.js'
+    path: path.resolve(__dirname, "dist"),
+    filename: "main.js",
   },
-  devtool: 'inline-source-map',
-  mode: 'development'
+  devtool: "inline-source-map",
+  mode: "development",
+  watch: true,
+  plugins: [new CopyPlugin([{ from: "src/index.html", to: "index.html" }])],
 };
 
 module.exports = config;
